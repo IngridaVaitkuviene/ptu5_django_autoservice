86b4900e4a2c9ca67a6bd2485d71bbed469f024a
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-11-10 08:57:58 +0200

    solved static, first template

--- ptu5_autoservice/autoservice/views.py
+++ ptu5_autoservice/autoservice/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Car, Service, Order
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, the autoservice is at Your services!")
+    # return HttpResponse("Hello, the autoservice is at Your services!")
+    service_count = Service.objects.count()
+    order_count = Order.objects.count()
+    car_count = Car.objects.count()
+
+    context = {
+        'service_count' : service_count,
+        'order_count' : order_count,
+        'car_count' : car_count
+    }
+
+    return render(request, 'autoservice/index.html', context)
