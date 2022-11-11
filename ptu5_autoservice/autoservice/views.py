df931618b830aaa8b6a5b5c16597892a45758d53
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-11-11 16:15:33 +0200

    added paginators, search entry

--- ptu5_autoservice/autoservice/views.py
+++ ptu5_autoservice/autoservice/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from . models import Car, Service, Order
 from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -19,7 +21,10 @@ def index(request):
     return render(request, 'autoservice/index.html', context)
 
 def cars(request):
-    return render(request, 'autoservice/cars.html', {'cars': Car.objects.all()})
+    paginator = Paginator(Car.objects.all(), 2)
+    page_number = request.GET.get('page')
+    paged_cars = paginator.get_page(page_number)
+    return render(request, 'autoservice/cars.html', {'cars': paged_cars})
 
 def car_info(request, car_id):
     return render(request, 'autoservice/car_info.html', {'car': get_object_or_404(Car, id=car_id)})
@@ -27,11 +32,19 @@ def car_info(request, car_id):
 
 class OrderlistView(ListView):
     model = Order
+    paginate_by = 3
     template_name = 'autoservice/order_list.html'
+
+
+    # kodel neveikia sitas def? klaida meta su get_context_data?????
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     search = self.request.GET.get('search')
+    #     if search:
+    #         queryset = queryset.filter(Q(title__icontains=search)| Q(summary__icontains=search))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['books_count'] = Book.objects.count()
         context['orders_count'] = self.get_queryset().count()
         return context
 
