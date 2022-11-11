b7f31c5c898c482bfbf9936c5a076d84e5d8625c
Ingrida Vaitkuviene <ingrida.strazdaite@gmail.com>
2022-11-11 11:06:22 +0200

    cars, car, orders, order urls

--- ptu5_autoservice/autoservice/urls.py
+++ ptu5_autoservice/autoservice/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('cars/', views.cars, name='cars'),
+    path('car/<int:car_id>/', views.car_info, name='car_info'),
+    path('orders/', views.OrderlistView.as_view(), name='orders'),
+    path('order/<int:pk>/', views.OrderDetailView.as_view(), name='order'),
 ]
